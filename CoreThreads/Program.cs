028284d567bc8a010b67f0cb146668168c6a5b33
233169-coder <233169@students.au.edu.pk>
2026-09-23 18:54:59 +0500

    Add files via upload

---
+++ CoreThreads/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Worker(object? arg)
+    {
+        int threadIndex = (int)arg!;
+
+        int processorId = Thread.GetCurrentProcessorId();
+
+        Console.WriteLine(
+            $"Thread {threadIndex}: running on logical processor {processorId}"
+        );
+    }
+
+    static void Main()
+    {
+
+        int numCores = Environment.ProcessorCount;
+
+        Console.WriteLine($"Detected logical cores: {numCores}");
+
+        Thread[] threads = new Thread[numCores];
+
+        for (int i = 0; i < numCores; i++)
+        {
+            int threadIndex = i;
+
+            threads[i] = new Thread(() => Worker(threadIndex));
+
+            threads[i].Start();
+        }
+
+        for (int i = 0; i < numCores; i++)
+        {
+            threads[i].Join();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Total threads created: {numCores}");
+        Console.WriteLine($"All {numCores} threads completed.");
+    }
+}
